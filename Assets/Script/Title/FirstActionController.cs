fe3f9521d9b767b3030e5ddf253bb56846a914c9
kurizono <myukino1213@gmail.com>
2021-06-22 21:15:12 +0900

    [add]セーブ&ロード

---
+++ Assets/Script/Title/FirstActionController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DataControll;
+using ReadTalk;
+
+public class FirstActionController : MonoBehaviour
+{
+    DataControll.SaveDataAccess datacs;
+    ReadTalk.TalkController talkcs;
+    static bool firstmethod = true;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        datacs = GetComponent<DataControll.SaveDataAccess>();
+        talkcs = GetComponent<ReadTalk.TalkController>();
+        if (firstmethod)
+        {
+            datacs.First();
+            talkcs.First();
+        }
+
+    }
+}
